6ee6f2516b1dbbbf54ab0c06b765401a0fdf7c5c
m-sameer040 <samkhanawan040@gmail.com>
2025-01-19 16:25:20 +0500

    first commit

---
+++ SanitySchema.js
@@ -0,0 +1,32 @@
+export default {
+    name: 'product',
+    type: 'document',
+    fields: [
+      {
+        name: 'name',
+        type: 'string',
+        title: 'Product Name'
+      },
+      {
+        name: 'price',
+        type: 'number',
+        title: 'Price'
+      },
+      {
+        name: 'stock',
+        type: 'number',
+        title: 'Stock Level'
+      },
+      {
+        name: 'description',
+        type: 'text',
+        title: 'Description'
+      },
+      {
+        name: 'image',
+        type: 'image',
+        title: 'Product Image'
+      }
+    ]
+  };
+  
